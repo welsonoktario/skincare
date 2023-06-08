e0f2948e841d53b0c6b3e6a01f49f37d796092a9
welsonoktario <welsonoktario@yahoo.com>
2023-06-09 02:34:55 +0700

    Fix title kategori admin

--- resources/views/admin/kategori/index.blade.php
+++ resources/views/admin/kategori/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
   <div class="container-fluid">
     <div class="d-sm-flex align-items-center justify-content-between my-4">
-      <h1 class="h3 mb-0 text-gray-800 d-none d-md-inline-block d-lg-inline-block d-xl-inline-block">Daftar Topup</h1>
+      <h1 class="h3 mb-0 text-gray-800 d-none d-md-inline-block d-lg-inline-block d-xl-inline-block">Kategori</h1>
       <button type="button" class="btn btn-primary btn-tambah">Tambah Kategori</button>
     </div>
     <div class="card shadow mb-3">
