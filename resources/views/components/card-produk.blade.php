28313c3da5b22fcf90776c0dafe6b37914aa6bf8
welsonoktario <welsonoktario@yahoo.com>
2023-06-11 14:08:06 +0700

    fix(card-produk): Atur tinggi foto'

--- resources/views/components/card-produk.blade.php
+++ resources/views/components/card-produk.blade.php
@@ -2,7 +2,8 @@
 
 <div class="px-2">
   <div class="card text-decoration-none card-hover px-0 mb-2">
-    <img class="card-img-top w-100" src="{{ $produk->placeholder }}" />
+    <img class="card-img-top w-100 object-fit-contain" src="{{ $produk->placeholder }}"
+      style="aspect-ratio: 1 / 1;" />
     <div class="card-body">
       <a href="{{ route('user.kategori.show', $produk->kategori_id) }}"
         class="link-info text-decoration-none">{{ $produk->kategori->nama }}</a>
